17ccb643f447941839bb46a80f9e8453fa900c1e
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2023-12-20 18:56:58 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "mcit_resource_group_420-ZZ5-UM"
+resource "azurerm_resource_group" "mcit420zz5umtest" {
+  name     = "mcit_resource_group_420-ZZ5-UMtest"
   location = "canadacentral"
 }
 
 /*
 resource "azurerm_storage_account" "azurermmcit" {
   name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
-  location                 = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name      = azurerm_resource_group.mcit420zz5umtest.name
+  location                 = azurerm_resource_group.mcit420zz5umtest.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
   access_tier="Cool"
